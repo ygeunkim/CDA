b33c3affbacf597799bdafc62da1689791d449ee
young-geun <dudrmshub@gmail.com>
2018-11-04 00:16:22 +0900

    fitting glm

--- _common.R
+++ _common.R
@@ -1,5 +1,10 @@
 set.seed(1011)
 
 knitr::opts_chunk$set(
-  comment = NULL
-)
+  comment = NA,
+  out.width = "70%",
+  fig.align = "center",
+  fig.width = 6,
+  fig.asp = .618,
+  fig.pos = "h"
+  )
